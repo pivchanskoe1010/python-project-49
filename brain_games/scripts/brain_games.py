f98667f2d322d50326358274105ffb3d86eb13c5
Yahor Zavinouski <pivchanskoe1010@gmail.com>
2023-06-13 16:30:56 +0200

    update project scripts

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,14 @@
+#!/usr/bin/env python3
+
+from brain_games.cli import *
+
 def greet():
     print(f'Welcome to the Brain Games!')
 
 def main():
     greet()
+    welcome_user()
 
 if __name__ == '__main__':
     main()
+
